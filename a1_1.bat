266683b12f35d5bbfe95da16efc98f60a2338ae8
bossm2 <51231093+bossm2@users.noreply.github.com>
2019-07-25 00:29:48 -0700

    my commit message

---
+++ a1_1.bat
@@ -0,0 +1,9 @@
+
+print_something () {
+echo Hello $1
+return 5
+}
+ff="aaaaaaaaaaaaaaaaaaaaaaa"
+#print_something $ff
+bb=$(print_something $ff)
+echo The previous function has a return value of $bb
